7eaf97fe39d870152a01829014c6da19b1333320
Johann Martinache <johann.martinache@massive.co>
2020-01-30 17:19:59 +0000

    feature : Cleaning deadcode
    Removing no more required code

--- internal/xcode/xcode_select.go
+++ internal/xcode/xcode_select.go
@@ -28,30 +28,16 @@ func NewSelectService(list ListService, exec util.Exec) *XCodeSelectService {
 }
 
 func (s *XCodeSelectService) SelectVersion(requirement string) (*Install, error) {
-	installs, err := s.list.List()
-	if err != nil {
-		return nil, err
-	}
-
+	// Should try to parse the required version
 	required, err := semver.Parse(requirement)
 	if err != nil {
 		return nil, ErrInvalidVersion
 	}
 
-	for _, install := range installs {
-		v, err := semver.Make(install.Version)
-		if err != nil {
-			logr.Error(err)
-			continue
-		}
+	// Find a equal match
+	target, err := s.findMatch(required, s.isEqualMatch)
 
-		if v.Equals(required) {
-			return install, nil
-		}
-	}
-
-	target, err := s.findMatch(requirement, s.isEqualMatch)
-
+	// In case of no match found
 	if target == nil || err != nil {
 		return nil, ErrMatchNotFound
 	}
@@ -59,27 +45,25 @@ func (s *XCodeSelectService) SelectVersion(requirement string) (*Install, error)
 	return target, nil
 }
 
-func (s *XCodeSelectService) isEqualMatch(install *Install, version string) (bool, error) {
-	r, err := semver.Parse(version)
-	if err != nil {
-		return false, err
-	}
-
+func (s *XCodeSelectService) isEqualMatch(install *Install, requirement semver.Version) (bool, error) {
 	if v, err := semver.Parse(install.Version); err == nil {
-		if v.Equals(r) {
+		if v.Equals(requirement) {
 			return true, nil
 		}
 	}
 	return false, nil
 }
 
-func (s *XCodeSelectService) findMatch(requirement string,
-	valid func(install *Install, version string) (bool, error)) (*Install, error) {
+func (s *XCodeSelectService) findMatch(requirement semver.Version,
+	valid func(install *Install, version semver.Version) (bool, error)) (*Install, error) {
+
+	// Resolve the list of candidates
 	list, err := s.list.List()
 	if err != nil {
 		return nil, err
 	}
 
+	// Iterate on installs
 	var installs = []*Install{}
 	for _, install := range list {
 		res, err := valid(install, requirement)
@@ -93,8 +77,10 @@ func (s *XCodeSelectService) findMatch(requirement string,
 		}
 	}
 
+	// Sort the installs
 	sortInstalls(installs)
 
+	// In case of no candidates
 	if len(installs) == 0 {
 		return nil, ErrMatchNotFound
 	}
@@ -104,19 +90,20 @@ func (s *XCodeSelectService) findMatch(requirement string,
 
 func sortInstalls(installs []*Install) {
 	sort.Slice(installs, func(i, j int) bool {
-		version1 := installs[i]
-		version2 := installs[j]
-
-		v1, err := semver.Parse(version1.Version)
-		if err != nil {
-			return false
-		}
-
-		v2, err := semver.Parse(version2.Version)
-		if err != nil {
-			return true
-		}
-
-		return v1.GT(v2)
+		return compareInstall(installs[i], installs[j])
 	})
+}
+
+func compareInstall(i1 *Install, i2 *Install) bool {
+	v1, err := semver.Parse(i1.Version)
+	if err != nil {
+		return false
+	}
+
+	v2, err := semver.Parse(i2.Version)
+	if err != nil {
+		return false
+	}
+
+	return v1.GT(v2)
 }
--- internal/xcode/xcode_select_test.go
+++ internal/xcode/xcode_select_test.go
@@ -3,6 +3,7 @@ package xcode
 import (
 	"fmt"
 
+	"github.com/blang/semver"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 
@@ -54,10 +55,15 @@ func mockResults() {
 			BundleVersion: "7.1-snapshot",
 			Version:       "7.1"},
 	}, nil)
-
 }
 
 func TestVersionSelection(t *testing.T) {
+	t.Run("Should handle invalid requirement", func(t *testing.T) {
+		install, err := subject.SelectVersion("Invalid")
+		assert.Nil(t, install)
+		assert.EqualError(t, err, ErrInvalidVersion.Error())
+	})
+
 	t.Run("If a error happens during the resolution, it should be reported", func(t *testing.T) {
 		setupSelectTest()
 		mockListService.On("List").
@@ -84,13 +90,14 @@ func TestVersionSelection(t *testing.T) {
 		assert.Nil(t, installl)
 	})
 
-	t.Run("Should expect an error in case of invalid version selection", func(t *testing.T) {
+	t.Run("Target resolution", func(t *testing.T) {
 		setupSelectTest()
 		mockResults()
-
-		install, err := subject.SelectVersion("Toto")
-		assert.EqualError(t, err, "Invalid version")
-		assert.Nil(t, install)
+		installl, err := subject.SelectVersion("10.3.1")
+		fmt.Println("install", installl)
+		assert.Nil(t, err)
+		assert.NotNil(t, installl)
+		assert.EqualValues(t, installl.Version, "10.3.1")
 	})
 }
 
@@ -98,28 +105,36 @@ func TestIsEqualMatch(t *testing.T) {
 	install := Install{Version: "10.2.3"}
 
 	t.Run("Matching should work as expected", func(t *testing.T) {
-		r, err := subject.isEqualMatch(&install, "10.2.3")
+		v1, _ := semver.Parse("10.2.3")
+		v2, _ := semver.Parse("10.2.0")
+
+		r, err := subject.isEqualMatch(&install, v1)
 		assert.True(t, r)
 		assert.NoError(t, err)
 
-		r, err = subject.isEqualMatch(&install, "10.2.0")
+		r, err = subject.isEqualMatch(&install, v2)
 		assert.False(t, r)
 		assert.NoError(t, err)
 	})
 
 	t.Run("Error should be handled", func(t *testing.T) {
-		r, err := subject.isEqualMatch(&install, "")
-		assert.Error(t, err, "t")
+		v3, _ := semver.Parse("")
+		r, err := subject.isEqualMatch(&install, v3)
+		assert.NoError(t, err)
 		assert.False(t, r)
 	})
 }
 
 func TestFindMatch(t *testing.T) {
+
+	required, err := semver.Parse("10.3.1")
+	assert.Nil(t, err)
+
 	t.Run("Valid match", func(t *testing.T) {
 		setupSelectTest()
 		mockResults()
 
-		install, err := subject.findMatch("10.3.1", subject.isEqualMatch)
+		install, err := subject.findMatch(required, subject.isEqualMatch)
 		assert.NotNil(t, install)
 		assert.NoError(t, err, "")
 	})
@@ -129,9 +144,24 @@ func TestFindMatch(t *testing.T) {
 		mockListService.On("List").
 			Return(nil, fmt.Errorf("Listing error"))
 
-		install, err := subject.findMatch("10.3.1", subject.isEqualMatch)
+		install, err := subject.findMatch(required, subject.isEqualMatch)
 		assert.Nil(t, install)
 		assert.EqualError(t, err, "Listing error")
+	})
+
+	t.Run("Should return ErrMatchNotFound in case of zero matchs", func(t *testing.T) {
+		setupSelectTest()
+
+		inst := &Install{}
+		mockListService.On("List").Return([]*Install{inst}, nil)
+
+		required2, err := semver.Parse("10.1.0")
+		assert.Nil(t, err)
+
+		install, err := subject.findMatch(required2, subject.isEqualMatch)
+		fmt.Println(install, err)
+		assert.Nil(t, install)
+		assert.EqualError(t, err, ErrMatchNotFound.Error())
 	})
 }
 
@@ -164,5 +194,21 @@ func TestSortInstalls(t *testing.T) {
 		assert.Equal(t, c[1], &install3)
 		assert.Equal(t, c[2], &install2)
 		assert.Equal(t, c[3], &empty)
+	})
+
+}
+
+func TestCompareInstall(t *testing.T) {
+	empty := Install{}
+	install := Install{Version: "10.1.2"}
+
+	t.Run("Should handle invalid install1", func(t *testing.T) {
+		res := compareInstall(&empty, &install)
+		assert.False(t, res)
+	})
+
+	t.Run("Should handle invalid install2", func(t *testing.T) {
+		res := compareInstall(&install, &empty)
+		assert.False(t, res)
 	})
 }
