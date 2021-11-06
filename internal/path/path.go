4a235c0993d4372f0718f9a4deb9e8a0b7270694
johann-martinache-deltatre <johann.martinache@deltatre.com>
2021-11-06 17:02:35 +0000

    feat(path): add workspace support
    add workspace and project output folder support

--- internal/path/path.go
+++ internal/path/path.go
@@ -4,9 +4,14 @@ import (
 	"dothething/internal/api"
 	"fmt"
 	"path/filepath"
+	"strings"
 )
 
-const BuildFolder = "../Build/"
+const (
+	BuildFolder      = "../Build/"
+	projectFileExt   = ".xcodeproj"
+	workspaceFileExt = ".xcworkspace"
+)
 
 type pathService struct {
 	*api.API
@@ -30,7 +35,7 @@ func (p pathService) Archive() string {
 	))
 }
 
-func (p pathService) ExportPlist() string {
+func (p pathService) ExportPList() string {
 	return filepath.Clean(filepath.Join(
 		p.buildFolder(),
 		fmt.Sprintf("%v-%v-%v-export.plist",
@@ -41,7 +46,22 @@ func (p pathService) ExportPlist() string {
 }
 
 func (p pathService) KeyChain() string {
-	return filepath.Join(p.buildFolder(), "do-the-thing.keychain")
+	res, err := filepath.Abs(filepath.Join(p.buildFolder(), "do-the-thing.keychain"))
+	if err != nil {
+		fmt.Printf("Error %v", err)
+	}
+
+	return res
+}
+
+func (p pathService) Package() string {
+	return filepath.Clean(filepath.Join(
+		p.buildFolder(),
+		fmt.Sprintf("%v-%v-%v.ipa",
+			p.API.Config.Target,
+			p.API.Config.Scheme,
+			p.API.Config.Configuration),
+	))
 }
 
 func (p pathService) ObjRoot() string {
@@ -50,6 +70,10 @@ func (p pathService) ObjRoot() string {
 
 func (p pathService) SymRoot() string {
 	return fmt.Sprintf("SYMROOT=%v", filepath.Join(p.buildFolder(), "sym/"))
+}
+
+func (p pathService) DerivedData() string {
+	return filepath.Join(p.buildFolder(), "derived-data/")
 }
 
 func (p pathService) XCResult() string {
@@ -61,4 +85,17 @@ func (p pathService) XCResult() string {
 			p.API.Config.Configuration,
 		),
 	)
+}
+
+func (p pathService) XCodeProject() string {
+	// TODO: Should we use the xcworkspacedata to resolve the project path?
+	if filepath.Ext(p.Config.Path) == workspaceFileExt {
+		return strings.TrimSuffix(p.Config.Path, workspaceFileExt) + projectFileExt
+	}
+
+	return p.Config.Path
+}
+
+func (p pathService) PBXProj() string {
+	return fmt.Sprintf("%v/project.pbxproj", p.XCodeProject())
 }
