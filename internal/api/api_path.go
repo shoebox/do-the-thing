691f12769d703156878860a6b0928019278fb215
johann-martinache-deltatre <johann.martinache@deltatre.com>
2021-11-06 17:38:43 +0000

    feat(path): adding build products paths definition

--- internal/api/api_path.go
+++ internal/api/api_path.go
@@ -2,8 +2,13 @@ package api
 
 type PathService interface {
 	Archive() string
+	DerivedData() string
+	ExportPList() string
 	KeyChain() string
 	ObjRoot() string
+	PBXProj() string
+	Package() string
 	SymRoot() string
 	XCResult() string
+	XCodeProject() string
 }
