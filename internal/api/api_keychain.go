7f36357b453011d16a2743c7db2ea429f1824c50
johann-martinache-deltatre <johann.martinache@deltatre.com>
2021-11-06 17:08:14 +0000

    chore(keychain): add certificate import

--- internal/api/api_keychain.go
+++ internal/api/api_keychain.go
@@ -5,6 +5,6 @@ import "context"
 type KeyChain interface {
 	Create(ctx context.Context, password string) error
 	Delete(ctx context.Context) error
-	ImportCertificate(ctx context.Context, filePath string, password string) error
+	ImportCertificate(ctx context.Context, filePath string, password string, commonName string) error
 	GetPath() string
 }
